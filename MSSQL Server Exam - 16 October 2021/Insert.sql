3122283a0b3cef0056c6b5fecc5662f31f63320c
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-06-18 19:34:41 +0300

    Create Insert.sql

---
+++ MSSQL Server Exam - 16 October 2021/Insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO Cigars(CigarName,BrandId,TastId,SizeId,PriceForSingleCigar,ImageURL)
+VALUES
+('COHIBA ROBUSTO',9,1,5,15.50,N'cohiba-robusto-stick_18.jpg'),
+('COHIBA SIGLO I',9,1,10,410.00,N'cohiba-siglo-i-stick_12.jpg'),
+('HOYO DE MONTERREY LE HOYO DU MAIRE',14,5,11,7.50,N'hoyo-du-maire-stick_17.jpg'),
+('HOYO DE MONTERREY LE HOYO DE SAN JUAN',14,4,15,32.00,N'hoyo-de-san-juan-stick_20.jpg'),
+('TRINIDAD COLONIALES',2,3,8,85.21,N'trinidad-coloniales-stick_30.jpg')
+
+INSERT INTO Addresses(Town,Country,Streat,ZIP)
+VALUES
+('Sofia',N'Bulgaria',N'18 Bul. Vasil levski',1000),
+('Athens',N'Greece',N'4342 McDonald Avenue',10435),
+('Zagreb',N'Croatia',N'4333 Lauren Drive',10000)
+
